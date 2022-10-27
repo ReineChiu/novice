d50ded7bddaad2733fe4fa2609b469298a7e7a69
ReineChiu <jodie0729@gmail.com>
2022-10-27 22:13:04 +0800

    use parameter

--- week-6/app.py
+++ week-6/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, request, render_template, redirect, session, url_for
 import mysql.connector
+from mysql_pm import web_select, web_insert
 import os
+from datetime import timedelta
+
 
 app=Flask(__name__,)
 
@@ -8,11 +11,12 @@ connection = mysql.connector.connect(
     host= "localhost",
     port= "3306",
     user="root",
-    password="*密碼隱藏馬賽克*",
+    password="隱藏ing",
     database="website"
 )
 
 app.config['SECRET_KEY'] = os.urandom(24)
+app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(days=1)
 
 @app.route("/")
 def index():
@@ -20,30 +24,25 @@ def index():
 
 @app.route("/signup",methods=["POST"])
 def signup():
-    regcursor=connection.cursor()
     name=request.form["name"]
     user=request.form["username"]
     psd=request.form["password"]
-    regcursor.execute("select * from member where username='"+user+"';")
-    account=regcursor.fetchall()
+    account=web_select(username=user)
     if account:
         return redirect(url_for("error",message="帳號已被註冊"))
     elif not name or not user or not psd:
         return redirect(url_for("error",message="姓名、帳號、密碼不能空白"))
     else:
-        regcursor.execute("insert into member(name, username, password) values('"+name+"','"+user+"','"+psd+"');")
-        connection.commit()
+        web_insert("member",name=name,username=user,password=psd)
         return redirect("/")
     cursor.close()
     connection.close()
 
 @app.route("/signin",methods=["POST"])
 def signin():
-    mycursor=connection.cursor()
     user=request.form["username"]
-    psd=request.form["password"]
-    mycursor.execute("select * from member where username='"+user+"' and password='"+psd+"';")
-    user=mycursor.fetchone()
+    psd=request.form["password"]   
+    user=web_select(username=user,password=psd)
     if user:
         session["userid"]=user[0]
         session["name"]=user[1]
@@ -53,7 +52,7 @@ def signin():
     else:  
         return redirect(url_for("error",message="帳號、或密碼輸入錯誤"))
     cursor.close()
-    connection.close()
+    connection.close() 
 
 @app.route("/member")
 def member():
@@ -71,7 +70,6 @@ def member():
 def message():
     mesgcursor=connection.cursor()
     member_id=session["userid"]
-    username=session["username"]
     content=request.form["mesgcontent"]
     mesgcursor.execute("insert into message(member_id, content) values('"+str(member_id)+"', '"+content+"');")
     mesg = mesgcursor.fetchall()
@@ -80,6 +78,7 @@ def message():
     cursor.close()
     connection.close()    
 
+
 @app.route("/error")
 def error():
     message=request.args.get("message","")
@@ -87,6 +86,7 @@ def error():
 
 @app.route("/signout")
 def signout():
+    session["loggedin"]=None
     session["userid"]=None
     session["name"]=None
     session["username"]=None
